17190a976c67cacfaada95be76be71592027ebaf
kenny <kennyticonah@gmail.com>
2026-05-16 16:17:34 +0000

    commit trabajo final

---
+++ accesoadministrador.py
@@ -0,0 +1,19 @@
+def acceso_usuario(username, password):
+    admin_user = "administrador" 
+    admin_pass = "1234"
+    
+    if username == admin_user and password == admin_pass:
+        print("Acceso concedido.")
+    else:
+        print("Usuario o contraseña incorrectos.")
+
+def main():
+    username = input("inserte usuario: ")
+    password = input("inserte password: ")
+  
+    acceso_usuario(username, password)
+
+    
+main() 
+
+
