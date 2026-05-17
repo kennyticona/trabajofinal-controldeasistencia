833c8031eea1a5fcd9f7167c6a0e43070e28674e
LisbethTorres <torresdagaroxana@gmail.com>
2026-05-17 18:24:32 -0500

    Update empleados.py

--- empleados.py
+++ empleados.py
@@ -1,3 +1,15 @@
+# VALIDAR SI EL EMPLEADO EXISTE
+def existe_empleado(nombre, dni_ingresado, empleados, dni):
+
+    for i in range(len(empleados)):
+
+        if empleados[i] == nombre and dni[i] == dni_ingresado:
+            return True
+
+    return False
+
+
+# BUSCAR EMPLEADO
 def buscar_empleado(dato, empleados, dni):
 
     encontrado = False
@@ -17,6 +29,7 @@ def buscar_empleado(dato, empleados, dni):
         print("Empleado no encontrado")
 
 
+# MOSTRAR TOTAL DE EMPLEADOS
 def total_empleados(empleados):
 
     print("Total de empleados registrados:", len(empleados))
