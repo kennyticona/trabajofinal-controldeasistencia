cda239fa25ada1eaabee52f82862bda72af41782
LisbethTorres <torresdagaroxana@gmail.com>
2026-05-17 17:08:30 -0500

    Se agregó búsqueda y total de empleados en subprograma empleados.py

--- empleados.py
+++ empleados.py
@@ -1,1 +1,22 @@
+def buscar_empleado(dato, empleados, dni):
 
+    encontrado = False
+
+    for i in range(len(empleados)):
+
+        if empleados[i] == dato or dni[i] == dato:
+
+            encontrado = True
+
+            print("Empleado encontrado")
+            print("Nombre:", empleados[i])
+            print("DNI:", dni[i])
+
+    if encontrado == False:
+
+        print("Empleado no encontrado")
+
+
+def total_empleados(empleados):
+
+    print("Total de empleados registrados:", len(empleados))
